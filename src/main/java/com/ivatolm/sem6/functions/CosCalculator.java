7511c05c0a3c29aff14f1d28de5d77643932b2cf
Ivan Tolmachev <ivatolm@gmail.com>
2025-05-02 13:34:01 +0300

    Rename test/unit to test/module. Add cos calc

---
+++ src/main/java/com/ivatolm/sem6/functions/CosCalculator.java
@@ -0,0 +1,17 @@
+package com.ivatolm.sem6.functions;
+
+import com.ivatolm.sem6.Calculator;
+
+public class CosCalculator implements Calculator {
+    private final SinCalculator sinCalculator;
+
+    public CosCalculator(SinCalculator sinCalculator) {
+        this.sinCalculator = sinCalculator;
+    }
+
+    @Override
+    public double calc(double x, double epsilon) {
+        double convertation = x + Math.PI / 2;
+        return sinCalculator.calc(convertation, epsilon);
+    }
+}
---
+++ src/test/java/com/ivatolm/sem6/stubs/CosCalculatorStub.java
@@ -0,0 +1,25 @@
+package com.ivatolm.sem6.stubs;
+
+import java.util.Map;
+
+import com.ivatolm.sem6.Calculator;
+
+public class CosCalculatorStub implements Calculator {
+    private static final Map<Double, Double> STUB_VALUES = Map.of(
+        0.0, 1.0,
+        Math.PI / 3, 0.5,
+        Math.PI / 2, 0.0,
+        Math.PI, -1.0,
+        2 * Math.PI / 3, -0.5,
+        3 * Math.PI / 2, 0.0,
+        2 * Math.PI, 1.0
+    );
+
+    @Override
+    public double calc(double x, double epsilon) {
+        if (!STUB_VALUES.containsKey(x)) {
+            System.out.println("WARNING: There is no stub value for x=" + x + " in cos calc");
+        }
+        return STUB_VALUES.getOrDefault(x, 0.0);
+    }
+}
